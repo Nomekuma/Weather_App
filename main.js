b9e03d4220dd3422b82f3e33c1011e25459e778a
github username <t.gunness35@gmail.com>
2023-03-26 19:36:32 +0400

    feat:fixed some AM:PM problem

--- main.js
+++ main.js
@@ -275,9 +275,9 @@ function getHour(time) {
     let min = time.split(":")[1];
     if (hour > 12 ) {
         hour = hour - 12;    
-        return `${hour}:${min}`;
+        return `${hour}:${min}am`;
     } else {
-        return `${hour}:${min}`;
+        return `${hour}:${min}pm`;
     }
 }
 
